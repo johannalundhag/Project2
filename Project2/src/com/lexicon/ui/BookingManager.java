12d28badde5a890adcf6b673fe989c25154591a7
Administrator <Administrator@DESK-7GZ7J32.lexicon.edu.local>
2017-07-12 10:55:39 +0200

    Added main and references to BookingManager

--- Project2/src/com/lexicon/ui/BookingManager.java
+++ Project2/src/com/lexicon/ui/BookingManager.java
@@ -2,5 +2,13 @@ package com.lexicon.ui;
 
 
 public class BookingManager {
+	
+	public BookingManager() {
+		
+	}
+	
+	public void start() {
+		
+	}
 
 }
---
+++ Project2/src/com/lexicon/ui/Main.java
@@ -0,0 +1,12 @@
+package com.lexicon.ui;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BookingManager app = new BookingManager();
+		app.start();
+
+	}
+
+}
