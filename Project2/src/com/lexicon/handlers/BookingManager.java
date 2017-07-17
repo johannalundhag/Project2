d643aa043acf80e22201c8c9038ef68f9efbfbf8
User <User@DESK-CGZ7J32.lexicon.edu.local>
2017-07-17 14:03:08 +0200

    More demoData added

--- Project2/src/com/lexicon/handlers/BookingManager.java
+++ Project2/src/com/lexicon/handlers/BookingManager.java
@@ -122,6 +122,20 @@ public class BookingManager {
 		FlightInformation fi1 = new FlightInformation("ARN", "LHR", date);
 		FlightInformation fi2 = new FlightInformation("ARN", "LAX", date);
 		FlightInformation fi3 = new FlightInformation("LHR", "LAX", date);
+		FlightInformation flight1 = new FlightInformation("ARN", "CPH", LocalDate.parse("2015-02-01"));
+		FlightInformation flight2 = new FlightInformation("CPH", "ARN", LocalDate.parse("2015-02-02"));
+		FlightInformation flight3 = new FlightInformation("ARN", "CPH", LocalDate.parse("2015-02-11"));
+		FlightInformation flight4 = new FlightInformation("CPH", "ARN", LocalDate.parse("2015-02-12"));
+		FlightInformation flight5 = new FlightInformation("ARN", "CPH", LocalDate.parse("2015-02-13"));
+		FlightInformation flight6 = new FlightInformation("CPH", "ARN", LocalDate.parse("2015-02-14"));
+
+		FlightInformation flight101 = new FlightInformation("ARN", "LHR", LocalDate.parse("2016-11-01"));
+		FlightInformation flight102 = new FlightInformation("LHR", "ARN", LocalDate.parse("2016-11-02"));
+		FlightInformation flight103 = new FlightInformation("ARN", "LHR", LocalDate.parse("2016-11-11"));
+		FlightInformation flight104 = new FlightInformation("LHR", "ARN", LocalDate.parse("2016-11-12"));
+		FlightInformation flight105 = new FlightInformation("ARN", "LHR", LocalDate.parse("2016-11-13"));
+		FlightInformation flight106 = new FlightInformation("LHR", "ARN", LocalDate.parse("2016-11-14"));
+		
 
 		Plane plane1 = new Plane(1, "747", 5, 5);
 		Plane plane2 = new Plane(1, "747", 10, 5);
@@ -150,6 +164,40 @@ public class BookingManager {
 		ticketList.add(ticket2);
 		ticketList.add(ticket3);
 		ticketList.add(ticket4);
+		
+		Ticket ticket11 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight1, null);
+		ticketList.add(ticket11);
+		Ticket ticket12 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight2, null);
+		ticketList.add(ticket12);
+		Ticket ticket13 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight3, null);
+		ticketList.add(ticket13);
+		Ticket ticket14 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight4, null);
+		ticketList.add(ticket14);
+		Ticket ticket5 = new Ticket(4000, new Customer("Cecilia Davidsson"), FlightClass.Business, flight5, null);
+		ticketList.add(ticket5);
+		Ticket ticket6 = new Ticket(4000, new Customer("Cecilia Davidsson"), FlightClass.Business, flight6, null);
+		ticketList.add(ticket6);
+		Ticket ticket7 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight5, null);
+		ticketList.add(ticket7);
+		Ticket ticket8 = new Ticket(1500, new Customer("Adam Bertilsson"), FlightClass.Economy, flight6, null);
+		ticketList.add(ticket8);
+
+		Ticket ticket101 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight101, null);
+		ticketList.add(ticket101);
+		Ticket ticket102 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight102, null);
+		ticketList.add(ticket102);
+		Ticket ticket103 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight103, null);
+		ticketList.add(ticket103);
+		Ticket ticket104 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight104, null);
+		ticketList.add(ticket104);
+		Ticket ticket105 = new Ticket(20000, new Customer("Cecilia Davidsson"), FlightClass.Business, flight105, null);
+		ticketList.add(ticket105);
+		Ticket ticket106 = new Ticket(20000, new Customer("Cecilia Davidsson"), FlightClass.Business, flight106, null);
+		ticketList.add(ticket106);
+		Ticket ticket107 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight105, null);
+		ticketList.add(ticket107);
+		Ticket ticket108 = new Ticket(5000, new Customer("Adam Bertilsson"), FlightClass.Economy, flight106, null);
+		ticketList.add(ticket108);	
 
 		tripList.get(0).setMenu(menu);
 		tripList.get(1).setMenu(menu);
